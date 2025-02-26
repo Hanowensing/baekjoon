eeb83745967a27602a454977f3f157dbd74f145b
Hanowens <hjy1627@naver.com>
2025-02-26 15:14:24 +0900

    '02/26'

---
+++ 백준/24267.py
@@ -0,0 +1,8 @@
+#C(n,e) 조합과 갯수가 같음!!
+
+n = int(input())
+
+print(int((n*(n-1)*(n-2))/6))
+print(3)
+
+
---
+++ 백준/24313.py
@@ -0,0 +1,14 @@
+a1, a0 = map(int,input().split())
+c= int(input())
+n0 = int(input())
+
+def f(a1,a0,n0):
+  return(a1*n0+a0)
+
+result = f(a1,a0,n0)
+
+
+if f(a1,a0,n0) <= c*n0 and a1<=c:
+  print(1)
+else:
+  print(0)
