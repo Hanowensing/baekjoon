291478b85089d5725f17dc13fe34392ffdcc34b6
Hanowens <hjy1627@naver.com>
2025-03-21 11:03:42 +0900

    '03/21'

---
+++ 백준/10845.py
@@ -0,0 +1,34 @@
+import sys  # 입력 속도 개선
+from collections import deque
+
+n = int(sys.stdin.readline().strip())
+list_1= deque() #입력 속도 개선
+
+for i in range(n):
+  a = sys.stdin.readline().strip().split()
+  if a[0] == 'push':
+    list_1.append(a[1])
+  elif a[0] == 'pop':
+    if len(list_1) == 0:
+      print(-1)
+    else:
+      #리스트 마지막 값 삭제에는 pop이 더 적절
+      #리스트 첫번쨰 값 삭제에는 pop left이 더 적절
+      print(list_1.popleft())
+  elif a[0] == 'size':
+    print(len(list_1))
+  elif a[0] == 'empty':
+    if len(list_1) == 0:
+      print(1)
+    else: 
+      print(0)
+  elif a[0] == 'front':
+    if len(list_1) ==0:
+      print(-1)
+    else:
+      print(list_1[0])
+  elif a[0] == 'back':
+    if len(list_1) ==0:
+      print(-1)
+    else:
+      print(list_1[-1])
